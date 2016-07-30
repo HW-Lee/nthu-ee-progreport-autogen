3c7413cb9fd832c413f8993ad95a5246471c5380
HW-Lee <tony123930@yahoo.com.tw>
2016-07-30 22:43:22 +0800

    add checking ip address

--- submit.py
+++ submit.py
@@ -1,6 +1,10 @@
 from rpautogen import ReportManager
+import socket
 
-rpmng = ReportManager("var.json")
-if rpmng.connect_server():
-	if rpmng.submit_progress("data.csv"):
-		print "Submit Success!"
+if socket.gethostbyname(socket.gethostname()).startswith("140.114"):
+	rpmng = ReportManager("var.json")
+	if rpmng.connect_server():
+		if rpmng.submit_progress("data.csv"):
+			print "Submit Success!"
+else:
+	print "The IP is not under NTHU."
