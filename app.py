f491db1e43581a8dde755f6187ad685a94b54ee7
HW-Lee <tony123930@yahoo.com.tw>
2016-08-08 01:54:04 +0800

    update configurations of deployment on heroku

--- app.py
+++ app.py
@@ -19,6 +19,6 @@ def get_index():
 def post_data():
     return make_response(jsonify(request.form), 200)
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
