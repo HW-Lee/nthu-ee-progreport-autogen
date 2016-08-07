e7bfdf4b7e2fd47b7c094d500d3b7cc020032427
HW-Lee <tony123930@yahoo.com.tw>
2016-08-08 00:17:46 +0800

    add service files

---
+++ app.py
@@ -0,0 +1,18 @@
+import json
+
+from flask import Flask, jsonify, abort, request, make_response, url_for
+
+from nthuee_prautogen.rpautogen import ReportManager
+
+app = Flask(__name__, static_url_path = "")
+
+@app.route("/api", methods=["GET"], strict_slashes=False)
+def get_index():
+    return make_response("This is the web-service implemented API of nthuee_prautogen.", 200)
+
+@app.route("/api/post", methods=["POST"], strict_slashes=False)
+def post_data():
+    return make_response(jsonify(request.form), 200)
+
+if __name__ == '__main__':
+    app.run(debug=True)
