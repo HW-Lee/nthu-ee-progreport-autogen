bf283b8fabde090d09f24e9190c8a796b0963b7f
HW-Lee <tony123930@yahoo.com.tw>
2016-08-08 16:32:43 +0800

    update /api/post/ route

--- app.py
+++ app.py
@@ -17,7 +17,36 @@ def get_index():
 
 @app.route("/api/post", methods=["POST"], strict_slashes=False)
 def post_data():
-    return make_response(jsonify(request.form), 200)
+    try:
+        usrvar = {
+            "vpn-username": request.form["vpn-username"],
+            "vpn-userpwd": request.form["vpn-userpwd"],
+            "username": request.form["username"],
+            "userpwd": request.form["userpwd"],
+            "studentname": request.form["studentname"]
+        }
+
+        data = {
+            "thisweek": request.form["thisweek"].encode("big5"),
+            "nextweek": request.form["nextweek"].encode("big5"),
+            "deadline": request.form["deadline"].encode("big5"),
+            "note": "submitted by ReportManager."
+        }
+
+    except:
+        return make_response("key-missing", 200)
+
+    resp = { "server-connected": False, "data-posted": False, "err-occur": False }
+    rpmng = ReportManager(usrvar)
+    if True:
+        if rpmng.connect_server(): 
+            resp["server-connected"] = True
+            if rpmng.submit_data(data): resp["data-posted"] = True
+
+    else:
+        resp["err-occur"] = True
+
+    return make_response(jsonify(resp), 200)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
